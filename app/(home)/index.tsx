f44915ad00b25eed52b1124cb12facc5985f49fb
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-07 17:30:36 +0500

    `Top Header Bar Done For Address`

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,18 +1,16 @@
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { Alert, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
-
+import {Octicons} from '@expo/vector-icons';
 type Props = {};
 
 const index = (props: Props) => {
-  const [locationServicesEnabled, setLocationServicesEnabled] = useState(false);
   const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
     "fetching your location ..."
   );
   async function getCurrentLocation() {
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") {
-      //   setErrorMsg('Permission to access location was denied')
       return;
     }
     console.log({ status });
@@ -22,7 +20,6 @@ const index = (props: Props) => {
     if (coords) {
       const { latitude, longitude } = coords;
 
-      //provide lat and long to get the the actual address
       let response = await Location.reverseGeocodeAsync({
         latitude,
         longitude
@@ -30,34 +27,56 @@ const index = (props: Props) => {
 
       let addressToDisplay;
 
-      console.log({response})
-
-    //   city , country , postalCode,
-
       if (response?.[0]?.formattedAddress) {
-        const addressToDisplay = `${response?.[0]?.formattedAddress.split(",").slice(1, -1)}, ${response?.[0]?.isoCountryCode}`;
+        const addressToDisplay = `${response?.[0]?.formattedAddress
+          .split(",")
+          .slice(1, -1)}, ${response?.[0]?.isoCountryCode}`;
 
         setDisplayCurrentAddress(addressToDisplay);
-
-      }else{
-
+      } else {
         addressToDisplay = `${response?.[0]?.streetNumber} ${response?.[0]?.street}, ${response?.[0]?.district}, ${response?.[0]?.city}, ${response?.[0]?.region}, ${response?.[0]?.isoCountryCode}`;
-        setDisplayCurrentAddress(addressToDisplay)
+        setDisplayCurrentAddress(addressToDisplay);
       }
-
-      //   }
-      //   setDisplayCurrentAddress(responce[0]?.city)
     }
   }
+
+  
 
   useEffect(() => {
     getCurrentLocation();
   }, []);
 
   return (
-    <View>
-      <Text>{displayCurrentAddress}</Text>
-    </View>
+    <ScrollView style={{ flex: 1, backgroundColor: "#f8f8f8" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 12,
+          padding: 10,
+        }}
+      >
+        <Octicons name="location" size={24} color="#E52850" />
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 15, fontWeight: "500" }}>Deliver To</Text>
+          <Text style={{ color: "gray", fontSize: 16, marginTop: 3 }}>
+            {displayCurrentAddress}
+          </Text>
+        </View>
+        <Pressable
+          style={{
+            backgroundColor: "#6CB4EE",
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>A</Text>
+        </Pressable>
+      </View>
+      </ScrollView>
   );
 };
 
